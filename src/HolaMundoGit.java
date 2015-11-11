0427f58eb5a7c92f00202e2186c6fa09107574a2
a071768530c <a071768530c@s09-pc22>
2015-11-11 20:16:49 +0100

    Cosas de en medio

--- src/HolaMundoGit.java
+++ src/HolaMundoGit.java
@@ -15,6 +15,7 @@ public class HolaMundoGit {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Cosas de en medio Mundo");
         System.out.println("Adios Mundo");
     }
     
